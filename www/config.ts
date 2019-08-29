55fedaab36a78846d801ba7a3b95fa5a31f60ea9
PluraCoin <36419889+pluracoin@users.noreply.github.com>
2019-08-29 20:33:05 +0200

    Config adapted to PLURA

--- www/config.ts
+++ www/config.ts
@@ -1,35 +1,35 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
-	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://wallet.karbo.org/api/',
-	mainnetExplorerUrl: "http://explorer.karbowanec.com/",
-	mainnetExplorerUrlHash: "http://explorer.karbowanec.com/?hash={ID}#blockchain_transaction",
-	mainnetExplorerUrlBlock: "http://explorer.karbowanec.com/?hash={ID}#blockchain_block",
-	testnetExplorerUrl: "http://testnet.karbo.org/",
-	testnetExplorerUrlHash: "http://testnet.karbo.org/?hash={ID}#blockchain_transaction",
-	testnetExplorerUrlBlock: "http://testnet.karbo.org/?hash={ID}#blockchain_block",
+	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://myplura.com/api/',
+	mainnetExplorerUrl: "https://explorer.pluracoin.org/",
+	mainnetExplorerUrlHash: "https://explorer.pluracoin.org/?hash={ID}#blockchain_transaction",
+	mainnetExplorerUrlBlock: "https://explorer.pluracoin.org/?hash={ID}#blockchain_block",
+	testnetExplorerUrl: "https://explorer.pluracoin.org/",
+	testnetExplorerUrlHash: "https://explorer.pluracoin.org/?hash={ID}#blockchain_transaction",
+	testnetExplorerUrlBlock: "https://explorer.pluracoin.org/?hash={ID}#blockchain_block",
 	testnet: false,
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 10,
     coinDisplayUnitPlaces: 2,
 	txMinConfirms: 10,         
 	txCoinbaseMinConfirms: 10,
-	addressPrefix: 111,
-	integratedAddressPrefix: 112,
-	addressPrefixTestnet: 111,
+	addressPrefix: 137,
+	integratedAddressPrefix: 137,
+	addressPrefixTestnet: 137,
 	integratedAddressPrefixTestnet: 112,
 	subAddressPrefix: 113,
 	subAddressPrefixTestnet: 113,
-	coinFee: new JSBigInt('100000000000'),
-	feePerKB: new JSBigInt('100000000000'), //for testnet its not used, as fee is dynamic.
+	coinFee: new JSBigInt('100000000'),
+	feePerKB: new JSBigInt('100000000'), //for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('100000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 0, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'KRB',
-	openAliasPrefix: "krb",
-	coinName: 'Karbo',
-	coinUriPrefix: 'karbowanec:',
-	avgBlockTime: 240,
+	coinSymbol: 'PLURA',
+	openAliasPrefix: "plura",
+	coinName: 'PluraCoin',
+	coinUriPrefix: 'pluracoin:',
+	avgBlockTime: 120,
 	maxBlockNumber: 500000000,
 };
